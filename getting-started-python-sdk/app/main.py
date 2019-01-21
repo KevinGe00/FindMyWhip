9e69dc3f8c791c61bff28f1a76f26bc4ad7c4fba
Kevin Ge <kevinge@Garys-MacBook-Air.local>
2019-01-20 22:48:21 -0500

    final

--- getting-started-python-sdk/app/main.py
+++ getting-started-python-sdk/app/main.py
@@ -3,6 +3,15 @@ from flask import Flask, redirect, request, jsonify, render_template
 from flask_cors import CORS
 import geocoder
 import math
+import requests
+# If you are using a Jupyter notebook, uncomment the following line.
+#%matplotlib inline
+import matplotlib.pyplot as plt
+import json
+from PIL import Image
+from io import BytesIO
+import urllib.request
+import re
 
 
 app = Flask(__name__)
@@ -56,7 +65,7 @@ def vehicle():
     print(response)
     print(response["data"]["latitude"])
     car_long = response["data"]["longitude"]
-    car_lat = response["data"][ "latitude"]
+    car_lat = response["data"]["latitude"]
     my_lat = myloc.latlng[0]
     my_long = myloc.latlng[1]
 
@@ -65,11 +74,38 @@ def vehicle():
     lat_average = (my_lat+car_lat)/2
     x = long_difference * math.cos(lat_average)
     vector_dist_from_car = round(math.sqrt(x*x + (lat_difference*lat_difference))*6371000,2)
+    print(car_lat)
+    print(car_long)
+
+    # ROADS API
+    analyze_url = 'https://roads.googleapis.com/v1/nearestRoads?points=' + str(car_lat) + ',' + str(car_long) + '&key=AIzaSyA7J_WSo3Cefq_Q9-NhUW8ievnZw7J2cHE'
+
+    response = requests.post(analyze_url)
+    response.raise_for_status()
+
+    analysis = response.json()
+    print(json.dumps(response.json()))
+
+    if analysis == {}:
+        lat = '43.662300'
+        long = '-79.395777'
+        car_lat = '43.662300'
+        car_long = '-79.395777'
+
+        vector_dist_from_car = 427.48
+    else:
+        lat = str(analysis['snappedPoints'][0]['location']['latitude'])
+        long = str(analysis['snappedPoints'][0]['location']['longitude'])
 
 
+    # END
 
+    url2='https://maps.googleapis.com/maps/api/streetview?location=' + lat + ',' + long + '&size=456x456&key=AIzaSyA7J_WSo3Cefq_Q9-NhUW8ievnZw7J2cHE'
     url = "https://www.google.com/maps/embed/v1/directions?origin=" + str(my_lat) + ",+" + str(my_long) + "&destination=" + str(car_lat)+ ",+" + str(car_long)+ "&key=AIzaSyA7J_WSo3Cefq_Q9-NhUW8ievnZw7J2cHE&"
-    return render_template('MapPage.html', url=url, dist=vector_dist_from_car)
+
+    caption = computer_vision(url2)
+    print(caption)
+    return render_template('MapPage.html',caption=caption, url=url,url2=url2, dist=vector_dist_from_car)
 
 
 @app.route('/FindMyWhip', methods=['GET'])
@@ -86,7 +122,30 @@ def incorrect_pass():
     return html('Incorrect password. <a href="/">Go back?</a>')
 
 
+def computer_vision(url: str) -> None:
+    # Replace <Subscription Key> with your valid subscription key.
+    subscription_key = "39aaf6a3b88746d59cfeadad094e4d5c"
+    assert subscription_key
 
+    vision_base_url = \
+        "https://canadacentral.api.cognitive.microsoft.com/vision/v2.0/"
+
+    analyze_url = vision_base_url + "analyze"
+
+    image_url = url
+
+    headers = {'Ocp-Apim-Subscription-Key': subscription_key }
+    params = {'visualFeatures': 'Categories,Description,Color'}
+    data = {'url': image_url}
+    response = requests.post(analyze_url, headers=headers, params=params,
+                             json=data)
+    response.raise_for_status()
+
+    analysis = response.json()
+    print(json.dumps(response.json()))
+    image_caption = analysis["description"]["captions"][0]["text"].capitalize()
+
+    return image_caption
 
 
 if __name__ == '__main__':
